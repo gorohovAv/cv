7e9180a789b27c917cccbb0371845958e1565ada
gorohovAv <gorohov.av@edu.spbstu.ru>
2024-06-08 18:33:07 +0300

    initial commit

---
+++ cv/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return(<div>
+            <p className='text-7xl text-grey font-bold'>About</p>
+            <p>Skills</p>
+            <p>Works</p>
+            <p>Contact me</p>
+           </div>
+    )
+};
+
+export default Header;
