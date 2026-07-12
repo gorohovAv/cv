d5ff5fcb613c66e87e3f163e75a1c0d6aa3a3e36
gorohovAv <gorohov.av@edu.spbstu.ru>
2026-07-12 21:43:46 +0300

    солнце

--- src/components/space.tsx
+++ src/components/space.tsx
@@ -236,8 +236,7 @@ export default function Space() {
         const hygText = await hygResponse.text()
         setLoading(true, 30, 'Parsing star catalog')
         
-        const stars = parseHYG(hygText)
-        setStars(stars)
+        let stars = parseHYG(hygText)
         setLoading(true, 50, 'Loading exoplanet database')
         
         // Load NASA exoplanet database
@@ -245,7 +244,79 @@ export default function Space() {
         const nasaText = await nasaResponse.text()
         setLoading(true, 80, 'Parsing exoplanet database')
         
-        const planets = parseNASAExoplanets(nasaText)
+        let planets = parseNASAExoplanets(nasaText)
+        
+        // Add Solar System programmatically
+        const sun: StarData = {
+          id: 0,
+          name: 'Sun',
+          ra: 0,
+          dec: 0,
+          dist: 0,
+          mag: 4.83, // Absolute magnitude for proper rendering size
+          spect: 'G2V',
+          x: 0,
+          y: 0,
+          z: 0,
+          gl: '',
+          hd: 'HD 1757',
+          hip: 'HIP 1757',
+        }
+        
+        const solarPlanetsData = [
+          { pl_name: 'Mercury', hostname: 'Sun', pl_orbsmax: 0.387, pl_orbper: 88.0, pl_rade: 0.383 },
+          { pl_name: 'Venus', hostname: 'Sun', pl_orbsmax: 0.723, pl_orbper: 224.7, pl_rade: 0.949 },
+          { pl_name: 'Earth', hostname: 'Sun', pl_orbsmax: 1.000, pl_orbper: 365.25, pl_rade: 1.000 },
+          { pl_name: 'Mars', hostname: 'Sun', pl_orbsmax: 1.524, pl_orbper: 687.0, pl_rade: 0.532 },
+          { pl_name: 'Jupiter', hostname: 'Sun', pl_orbsmax: 5.203, pl_orbper: 4331, pl_rade: 11.209 },
+          { pl_name: 'Saturn', hostname: 'Sun', pl_orbsmax: 9.537, pl_orbper: 10747, pl_rade: 9.449 },
+          { pl_name: 'Uranus', hostname: 'Sun', pl_orbsmax: 15.191, pl_orbper: 30589, pl_rade: 4.007 },
+          { pl_name: 'Neptune', hostname: 'Sun', pl_orbsmax: 20.069, pl_orbper: 59800, pl_rade: 3.883 },
+        ]
+        
+        const solarPlanets = solarPlanetsData.map(p => ({
+          pl_name: p.pl_name,
+          hostname: p.hostname,
+          default_flag: 1,
+          sy_snum: 1,
+          sy_pnum: 1,
+          discoverymethod: '',
+          disc_year: 0,
+          disc_facility: '',
+          soltype: '',
+          pl_controv_flag: 0,
+          pl_refname: '',
+          pl_orbper: p.pl_orbper,
+          pl_orbpererr1: 0, pl_orbpererr2: 0, pl_orbperlim: 0,
+          pl_orbsmax: p.pl_orbsmax,
+          pl_orbsmaxerr1: 0, pl_orbsmaxerr2: 0, pl_orbsmaxlim: 0,
+          pl_rade: p.pl_rade,
+          pl_radeerr1: 0, pl_radeerr2: 0, pl_radelim: 0,
+          pl_radj: 0, pl_radjerr1: 0, pl_radjerr2: 0, pl_radjlim: 0,
+          pl_bmasse: 0, pl_bmasseerr1: 0, pl_bmasseerr2: 0, pl_bmasselim: 0,
+          pl_bmassj: 0, pl_bmassjerr1: 0, pl_bmassjerr2: 0, pl_bmassjlim: 0,
+          pl_bmassprov: '',
+          pl_orbeccen: 0, pl_orbeccenerr1: 0, pl_orbeccenerr2: 0, pl_orbeccenlim: 0,
+          pl_insol: 0, pl_insolerr1: 0, pl_insolerr2: 0, pl_insollim: 0,
+          pl_eqt: 0, pl_eqterr1: 0, pl_eqterr2: 0, pl_eqtlim: 0,
+          ttv_flag: 0,
+          st_refname: '', st_spectype: 'G2V', st_teff: 5778, st_tefferr1: 0, st_tefferr2: 0, st_tefflim: 0,
+          st_rad: 1, st_raderr1: 0, st_raderr2: 0, st_radlim: 0,
+          st_mass: 1, st_masserr1: 0, st_masserr2: 0, st_masslim: 0,
+          st_met: 0, st_meterr1: 0, st_meterr2: 0, st_metlim: 0, st_metratio: '',
+          st_logg: 0, st_loggerr1: 0, st_loggerr2: 0, st_logglim: 0,
+          sy_refname: '', rastr: '', ra: 0, decstr: '', dec: 0,
+          sy_dist: 0, sy_disterr1: 0, sy_disterr2: 0,
+          sy_vmag: 0, sy_vmagerr1: 0, sy_vmagerr2: 0,
+          sy_kmag: 0, sy_kmagerr1: 0, sy_kmagerr2: 0,
+          sy_gaiamag: 0, sy_gaiamagerr1: 0, sy_gaiamagerr2: 0,
+          rowupdate: '', pl_pubdate: '', releasedate: '',
+        } as PlanetData))
+        
+        stars = [sun, ...stars]
+        planets = [...planets, ...solarPlanets]
+        
+        setStars(stars)
         setPlanets(planets)
         
         setLoading(true, 100, 'Initializing space')
